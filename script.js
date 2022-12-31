707fb042549744cc93e43b50e2efece6ad1c58dd
Alexandre <alexmcs@gmail.com>
2022-12-31 14:16:44 -0300

    Implementando funções dos botões.

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ const prevBtns = document.querySelectorAll(".btn-prev");
 const nextBtns = document.querySelectorAll(".btn-next");
 const progress = document.getElementById("progress");
 const formSteps = document.querySelectorAll(".form-step");
+
+let formStepsNum = 0;
+
+nextBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        formStepsNum++;
+        updateFormSteps();
+    })
+})
+
+prevBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        formStepsNum--;
+        updateFormSteps();
+    })
+})
+
+function updateFormSteps(){
+    formSteps.forEach((formStep) => {
+        formStep.classList.contains("form-step-active") &&
+            formStep.classList.remove("form-step-active");
+    });
+
+    formSteps[formStepsNum].classList.add("form-step-active");
+}
