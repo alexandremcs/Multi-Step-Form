359e609b827537bcd10778f30b119ad05710a95f
Alexandre <alexmcs@gmail.com>
2023-01-01 11:25:17 -0300

    Implementando código do progresso.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const prevBtns = document.querySelectorAll(".btn-prev");
 const nextBtns = document.querySelectorAll(".btn-next");
 const progress = document.getElementById("progress");
 const formSteps = document.querySelectorAll(".form-step");
+const progressSteps = document.querySelectorAll(".progress-step")
 
 let formStepsNum = 0;
 
@@ -9,6 +10,7 @@ nextBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         formStepsNum++;
         updateFormSteps();
+        updateProgressBar();
     })
 })
 
@@ -16,6 +18,7 @@ prevBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         formStepsNum--;
         updateFormSteps();
+        updateProgressBar();
     })
 })
 
@@ -26,4 +29,14 @@ function updateFormSteps(){
     });
 
     formSteps[formStepsNum].classList.add("form-step-active");
+}
+
+function updateProgressBar(){
+    progressSteps.forEach((progressStep, index) => {
+        if(index < formStepsNum + 1) {
+            progressStep.classList.add('progress-step-active');
+        } else {
+            progressStep.classList.remove('progress-step-active');
+        }
+    })
 }
